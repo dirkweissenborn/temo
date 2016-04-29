147a8c15bf8deefabd26a882be7392d3d755a65c
Dirk Weissenborn <dirk.weissenborn@gmail.com>
2016-04-29 18:45:31 +0200

    adaptation of translate scripts from tensorflow removes bucketing. test_run working

---
+++ translate.py
@@ -0,0 +1,229 @@
+# THIS IS AN ADAPTION FROM THE ORIGINAL translate.py OF TF
+
+"""Binary for training translation models and decoding from them.
+Running this program without --decode will download the WMT corpus into
+the directory specified as --data_dir and tokenize it in a very basic way,
+and then start training a model saving checkpoints to --train_dir.
+Running with --decode starts an interactive loop so you can see how
+the current checkpoint translates English sentences into French.
+See the following papers for more information on neural translation models.
+ * http://arxiv.org/abs/1409.3215
+ * http://arxiv.org/abs/1409.0473
+ * http://arxiv.org/abs/1412.2007
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import math
+import os
+import random
+import sys
+import time
+
+import numpy as np
+import tensorflow as tf
+
+from wmt14 import data_utils
+from wmt14 import seq2seq_model
+
+
+tf.app.flags.DEFINE_float("learning_rate", 0.5, "Learning rate.")
+tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.99,
+                          "Learning rate decays by this much.")
+tf.app.flags.DEFINE_float("max_gradient_norm", 5.0,
+                          "Clip gradients to this norm.")
+tf.app.flags.DEFINE_integer("batch_size", 64,
+                            "Batch size to use during training.")
+tf.app.flags.DEFINE_integer("size", 1024, "Size of each model layer.")
+tf.app.flags.DEFINE_integer("num_layers", 3, "Number of layers in the model.")
+tf.app.flags.DEFINE_integer("en_vocab_size", 40000, "English vocabulary size.")
+tf.app.flags.DEFINE_integer("fr_vocab_size", 40000, "French vocabulary size.")
+tf.app.flags.DEFINE_string("data_dir", "/tmp", "Data directory")
+tf.app.flags.DEFINE_string("train_dir", "/tmp", "Training directory.")
+tf.app.flags.DEFINE_integer("max_train_data_size", 0,
+                            "Limit on the size of training data (0: no limit).")
+tf.app.flags.DEFINE_integer("steps_per_checkpoint", 200,
+                            "How many training steps to do per checkpoint.")
+tf.app.flags.DEFINE_boolean("decode", False,
+                            "Set to True for interactive decoding.")
+tf.app.flags.DEFINE_boolean("self_test", False,
+                            "Run a self-test if this is set to True.")
+
+FLAGS = tf.app.flags.FLAGS
+
+
+def read_data(source_path, target_path, max_length, max_size=None):
+    """Read data from source and target files and put into buckets.
+    Args:
+      source_path: path to the files with token-ids for the source language.
+      target_path: path to the file with token-ids for the target language;
+        it must be aligned with the source file: n-th line contains the desired
+        output for n-th line from the source_path.
+      max_size: maximum number of lines to read, all other will be ignored;
+        if 0 or None, data files will be read completely (no limit).
+    Returns:
+      data_set: a list of length len(_buckets); data_set[n] contains a list of
+        (source, target) pairs read from the provided data files that fit
+        into the n-th bucket, i.e., such that len(source) < _buckets[n][0] and
+        len(target) < _buckets[n][1]; source and target are lists of token-ids.
+    """
+    data_set = []
+    m_length = 0
+    with tf.gfile.GFile(source_path, mode="r") as source_file:
+        with tf.gfile.GFile(target_path, mode="r") as target_file:
+            source, target = source_file.readline(), target_file.readline()
+            counter = 0
+            while source and target and (not max_size or counter < max_size):
+                counter += 1
+                if counter % 100000 == 0:
+                    print("  reading data line %d" % counter)
+                    sys.stdout.flush()
+                source_ids = [int(x) for x in source.split()]
+                target_ids = [int(x) for x in target.split()]
+                target_ids.append(data_utils.EOS_ID)
+                if len(source_ids) < max_length and len(target_ids) < max_length:
+                    m_length = max(len(target_ids), max(len(source_ids), m_length))
+                    data_set.append([source_ids, target_ids])
+                if max_size is not None and counter >= max_size:
+                    break
+    return data_set, m_length
+
+
+def create_model(session, forward_only, max_length):
+    """Create translation model and initialize or load parameters in session."""
+    model = seq2seq_model.Seq2SeqModel(
+        FLAGS.en_vocab_size, FLAGS.fr_vocab_size, max_length,
+        FLAGS.size, FLAGS.num_layers, FLAGS.max_gradient_norm, FLAGS.batch_size,
+        FLAGS.learning_rate, FLAGS.learning_rate_decay_factor,
+        forward_only=forward_only)
+    ckpt = tf.train.get_checkpoint_state(FLAGS.train_dir)
+    if ckpt and tf.gfile.Exists(ckpt.model_checkpoint_path):
+        print("Reading model parameters from %s" % ckpt.model_checkpoint_path)
+        model.saver.restore(session, ckpt.model_checkpoint_path)
+    else:
+        print("Created model with fresh parameters.")
+        session.run(tf.initialize_all_variables())
+    return model
+
+
+def train():
+    """Train a en->fr translation model using WMT data."""
+    # Prepare WMT data.
+    print("Preparing WMT data in %s" % FLAGS.data_dir)
+    en_train, fr_train, en_dev, fr_dev, en_test, fr_test, _, _ = data_utils.prepare_wmt_data(
+        FLAGS.data_dir, FLAGS.en_vocab_size, FLAGS.fr_vocab_size)
+
+    with tf.Session() as sess:
+        # Read data.
+        print ("Reading development and training data (limit: %d)."
+               % FLAGS.max_train_data_size)
+        dev_set, max_length = read_data(en_dev, fr_dev, FLAGS.max_length)
+        train_set, l = read_data(en_train, fr_train, FLAGS.max_length, FLAGS.max_train_data_size)
+        max_length = max(l, max_length)
+        train_total_size = len(train_set)
+
+        print("Creating %d layers of %d units." % (FLAGS.num_layers, FLAGS.size))
+        model = create_model(sess, False, max_length)
+
+        # This is the training loop.
+        step_time, loss = 0.0, 0.0
+        current_step = 0
+        previous_losses = []
+        while True:
+            # Get a batch and make a step.
+            start_time = time.time()
+            encoder_inputs, decoder_inputs, encoder_length, decoder_length = model.get_batch(train_set)
+            _, step_loss, _ = model.step(sess, encoder_inputs, decoder_inputs, encoder_length, decoder_length, False)
+            step_time += (time.time() - start_time) / FLAGS.steps_per_checkpoint
+            loss += step_loss / FLAGS.steps_per_checkpoint
+            current_step += 1
+
+            # Once in a while, we save checkpoint, print statistics, and run evals.
+            if current_step % FLAGS.steps_per_checkpoint == 0:
+                # Print statistics for the previous epoch.
+                perplexity = math.exp(loss) if loss < 300 else float('inf')
+                print ("global step %d learning rate %.4f step-time %.2f perplexity "
+                       "%.2f" % (model.global_step.eval(), model.learning_rate.eval(),
+                                 step_time, perplexity))
+                # Decrease learning rate if no improvement was seen over last 3 times.
+                if len(previous_losses) > 2 and loss > max(previous_losses[-3:]):
+                    sess.run(model.learning_rate_decay_op)
+                previous_losses.append(loss)
+                # Save checkpoint and zero timer and loss.
+                checkpoint_path = os.path.join(FLAGS.train_dir, "translate.ckpt")
+                model.saver.save(sess, checkpoint_path, global_step=model.global_step)
+                step_time, loss = 0.0, 0.0
+                # Run evals on development set and print their perplexity.
+                encoder_inputs, decoder_inputs, encoder_length, decoder_length = model.get_batch(dev_set)
+                _, eval_loss, _ = model.step(sess, encoder_inputs, decoder_inputs,
+                                             encoder_length, decoder_length, True)
+                eval_ppx = math.exp(eval_loss) if eval_loss < 300 else float('inf')
+                print("  eval perplexity %.2f" % eval_ppx)
+                sys.stdout.flush()
+
+
+def decode():
+    with tf.Session() as sess:
+        # Create model and load parameters.
+        model = create_model(sess, True, FLAGS.max_length)
+        model.batch_size = 1  # We decode one sentence at a time.
+
+        # Load vocabularies.
+        en_vocab_path = os.path.join(FLAGS.data_dir,
+                                     "vocab%d.en" % FLAGS.en_vocab_size)
+        fr_vocab_path = os.path.join(FLAGS.data_dir,
+                                     "vocab%d.fr" % FLAGS.fr_vocab_size)
+        en_vocab, _ = data_utils.initialize_vocabulary(en_vocab_path)
+        _, rev_fr_vocab = data_utils.initialize_vocabulary(fr_vocab_path)
+
+        # Decode from standard input.
+        sys.stdout.write("> ")
+        sys.stdout.flush()
+        sentence = sys.stdin.readline()
+        while sentence:
+            # Get token-ids for the input sentence.
+            token_ids = data_utils.sentence_to_token_ids(tf.compat.as_bytes(sentence), en_vocab)
+            # Get a 1-element batch to feed the sentence to the model.
+            encoder_inputs, decoder_inputs, encoder_length, decoder_length = model.get_batch([(token_ids, [])])
+            # Get output logits for the sentence.
+            _, _, output_logits = model.step(sess, encoder_inputs, decoder_inputs,
+                                             encoder_length, decoder_length, True)
+            # This is a greedy decoder - outputs are just argmaxes of output_logits.
+            outputs = [int(np.argmax(logit, axis=1)) for logit in output_logits]
+            # If there is an EOS symbol in outputs, cut them at that point.
+            if data_utils.EOS_ID in outputs:
+                outputs = outputs[:outputs.index(data_utils.EOS_ID)]
+            # Print out French sentence corresponding to outputs.
+            print(" ".join([tf.compat.as_str(rev_fr_vocab[output]) for output in outputs]))
+            print("> ", end="")
+            sys.stdout.flush()
+            sentence = sys.stdin.readline()
+
+
+def self_test():
+    """Test the translation model."""
+    with tf.Session() as sess:
+        print("Self-test for neural translation model.")
+        # Create model with vocabularies of 10, 2 small buckets, 2 layers of 32.
+        model = seq2seq_model.Seq2SeqModel(10, 10, 6, 32, 2,
+                                           5.0, 32, 0.3, 0.99, num_samples=8)
+        sess.run(tf.initialize_all_variables())
+
+        # Fake data set for both the (3, 3) and (6, 6) bucket.
+        data_set = [([1, 1], [2, 2]), ([3, 3], [4]), ([5], [6]), ([1, 1, 1, 1, 1], [2, 2, 2, 2, 2]), ([3, 3, 3], [5, 6])]
+        for _ in range(5):  # Train the fake model for 5 steps.
+            encoder_inputs, decoder_inputs, encoder_length, decoder_length = model.get_batch(data_set)
+            model.step(sess, encoder_inputs, decoder_inputs, encoder_length, decoder_length, False)
+
+
+def main(_):
+    if FLAGS.self_test:
+        self_test()
+    elif FLAGS.decode:
+        decode()
+    else:
+        train()
+
+if __name__ == "__main__":
+    tf.app.run()
---
+++ wmt14/seq2seq_model.py
@@ -0,0 +1,255 @@
+# Adaption from original TF code
+
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Sequence-to-sequence model with an attention mechanism."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import random
+
+import numpy as np
+import tensorflow as tf
+
+from wmt14 import data_utils
+from wmt14 import my_seq2seq
+
+
+
+class Seq2SeqModel(object):
+    """Sequence-to-sequence model with attention and for multiple buckets.
+
+    This class implements a multi-layer recurrent neural network as encoder,
+    and an attention-based decoder. This is the same as the model described in
+    this paper: http://arxiv.org/abs/1412.7449 - please look there for details,
+    or into the seq2seq library for complete model implementation.
+    This class also allows to use GRU cells in addition to LSTM cells, and
+    sampled softmax to handle large output vocabulary size. A single-layer
+    version of this model, but with bi-directional encoder, was presented in
+      http://arxiv.org/abs/1409.0473
+    and sampled softmax is described in Section 3 of the following paper.
+      http://arxiv.org/abs/1412.2007
+    """
+
+    def __init__(self, source_vocab_size, target_vocab_size, max_length, size,
+                 num_layers, max_gradient_norm, batch_size, learning_rate,
+                 learning_rate_decay_factor, cell_type="GRU",
+                 num_samples=512, forward_only=False):
+        """Create the model.
+
+        Args:
+          source_vocab_size: size of the source vocabulary.
+          target_vocab_size: size of the target vocabulary.
+          max_length: max_length of source or target sentence
+          size: number of units in each layer of the model.
+          num_layers: number of layers in the model.
+          max_gradient_norm: gradients will be clipped to maximally this norm.
+          batch_size: the size of the batches used during training;
+            the model construction is independent of batch_size, so it can be
+            changed after initialization if this is convenient, e.g., for decoding.
+          learning_rate: learning rate to start with.
+          learning_rate_decay_factor: decay learning rate by this much when needed.
+          use_lstm: if true, we use LSTM cells instead of GRU cells.
+          num_samples: number of samples for sampled softmax.
+          forward_only: if set, we do not construct the backward pass in the model.
+        """
+        self.source_vocab_size = source_vocab_size
+        self.target_vocab_size = target_vocab_size
+        self.max_length = max_length
+        self.batch_size = batch_size
+        self.learning_rate = tf.Variable(float(learning_rate), trainable=False)
+        self.learning_rate_decay_op = self.learning_rate.assign(
+            self.learning_rate * learning_rate_decay_factor)
+        self.global_step = tf.Variable(0, trainable=False)
+
+        # If we use sampled softmax, we need an output projection.
+        output_projection = None
+        softmax_loss_function = None
+        # Sampled softmax only makes sense if we sample less than vocabulary size.
+        if num_samples > 0 and num_samples < self.target_vocab_size:
+            with tf.device("/cpu:0"):
+                w = tf.get_variable("proj_w", [size, self.target_vocab_size])
+                w_t = tf.transpose(w)
+                b = tf.get_variable("proj_b", [self.target_vocab_size])
+            output_projection = (w, b)
+
+            def sampled_loss(inputs, labels):
+                with tf.device("/cpu:0"):
+                    labels = tf.reshape(labels, [-1, 1])
+                    return tf.nn.sampled_softmax_loss(w_t, b, inputs, labels, num_samples,
+                                                      self.target_vocab_size)
+            softmax_loss_function = sampled_loss
+
+        # Create the internal multi-layer cell for our RNN.
+        single_cell = None
+        if cell_type == "GRU":
+            single_cell = tf.nn.rnn_cell.GRUCell(size)
+        else:
+            single_cell = tf.nn.rnn_cell.BasicLSTMCell(size)
+
+        cell = single_cell
+        if num_layers > 1:
+            cell = tf.nn.rnn_cell.MultiRNNCell([single_cell] * num_layers)
+
+        # The seq2seq function: we use embedding for the input and attention.
+        def seq2seq_f(encoder_inputs, decoder_inputs, encoder_length, decoder_length, do_decode):
+            return my_seq2seq.embedding_attention_seq2seq(
+                encoder_inputs, decoder_inputs, encoder_length, decoder_length, cell,
+                num_encoder_symbols=source_vocab_size,
+                num_decoder_symbols=target_vocab_size,
+                embedding_size=size,
+                output_projection=output_projection,
+                feed_previous=do_decode)
+
+        # Feeds for inputs.
+        self.encoder_inputs = []
+        self.decoder_inputs = []
+        self.encoder_length = tf.placeholder(tf.int32, shape=[None], name="encoder_length")
+        self.decoder_length = tf.placeholder(tf.int32, shape=[None], name="decoder_length")
+        for i in range(max_length):  # Last bucket is the biggest one.
+            self.encoder_inputs.append(tf.placeholder(tf.int32, shape=[None],
+                                                      name="encoder{0}".format(i)))
+        for i in range(max_length):
+            self.decoder_inputs.append(tf.placeholder(tf.int32, shape=[None],
+                                                      name="decoder{0}".format(i)))
+
+        # Our targets are decoder inputs shifted by one.
+        targets = [self.decoder_inputs[i + 1]
+                   for i in range(len(self.decoder_inputs) - 1)]
+
+        # Training outputs and losses.
+        if forward_only:
+            self.outputs, _ = seq2seq_f(self.encoder_inputs, self.decoder_inputs,
+                                        self.encoder_length, self.decoder_length, True)
+        else:
+            self.outputs, _ = seq2seq_f(self.encoder_inputs, self.decoder_inputs,
+                                        self.encoder_length, self.decoder_length, False)
+
+        self.loss = my_seq2seq.sequence_loss(self.outputs[:-1], targets, self.decoder_length,
+                                softmax_loss_function=softmax_loss_function)
+
+        # Gradients and SGD update operation for training the model.
+        params = tf.trainable_variables()
+        if not forward_only:
+            opt = tf.train.AdamOptimizer(self.learning_rate, beta1=0.0)
+            gradients = tf.gradients(self.loss, params)
+            clipped_gradients, self.gradient_norm = tf.clip_by_global_norm(gradients,
+                                                             max_gradient_norm)
+            self.update = opt.apply_gradients(zip(clipped_gradients, params), global_step=self.global_step)
+        self.saver = tf.train.Saver(tf.all_variables())
+
+    def step(self, session, encoder_inputs, decoder_inputs, encoder_length, decoder_length, forward_only):
+        """Run a step of the model feeding the given inputs.
+
+        Args:
+          session: tensorflow session to use.
+          encoder_inputs: list of numpy int vectors to feed as encoder inputs.
+          decoder_inputs: list of numpy int vectors to feed as decoder inputs.
+          target_weights: list of numpy float vectors to feed as target weights.
+          bucket_id: which bucket of the model to use.
+          forward_only: whether to do the backward step or only forward.
+
+        Returns:
+          A triple consisting of gradient norm (or None if we did not do backward),
+          average perplexity, and the outputs.
+
+        Raises:
+          ValueError: if length of encoder_inputs, decoder_inputs, or
+            target_weights disagrees with bucket size for the specified bucket_id.
+        """
+        # Check if the sizes match.
+        encoder_size, decoder_size = max(encoder_length), max(decoder_length)
+
+        # Input feed: encoder inputs, decoder inputs, target_weights, as provided.
+        input_feed = {}
+        for l in range(encoder_size):
+            input_feed[self.encoder_inputs[l].name] = encoder_inputs[l]
+        for l in range(decoder_size):
+            input_feed[self.decoder_inputs[l].name] = decoder_inputs[l]
+        for l in range(encoder_size, self.max_length):
+            input_feed[self.encoder_inputs[l].name] = decoder_inputs[-1]
+        for l in range(decoder_size, self.max_length):
+            input_feed[self.decoder_inputs[l].name] = encoder_inputs[-1]
+
+        input_feed[self.encoder_length.name] = encoder_length
+        input_feed[self.decoder_length.name] = decoder_length
+
+        # Output feed: depends on whether we do a backward step or not.
+        if not forward_only:
+            output_feed = [self.update,  # Update Op that does SGD.
+                           self.gradient_norm,  # Gradient norm.
+                           self.loss]  # Loss for this batch.
+        else:
+            output_feed = [self.loss]  # Loss for this batch.
+            for l in range(decoder_size):  # Output logits.
+                output_feed.append(self.outputs[l])
+
+        outputs = session.run(output_feed, input_feed)
+        if not forward_only:
+            return outputs[1], outputs[2], None  # Gradient norm, loss, no outputs.
+        else:
+            return None, outputs[0], outputs[1:]  # No gradient norm, loss, outputs.
+
+    def get_batch(self, data):
+        """Get a random batch of data from the specified bucket, prepare for step.
+
+        To feed data in step(..) it must be a list of batch-major vectors, while
+        data here contains single length-major cases. So the main logic of this
+        function is to re-index data cases to be in the proper format for feeding.
+
+        Args:
+          data: a tuple of size len(self.buckets) in which each element contains
+            lists of pairs of input and output data that we use to create a batch.
+
+        Returns:
+          The triple (encoder_inputs, decoder_inputs, target_weights) for
+          the constructed batch that has the proper format to call step(...) later.
+        """
+        encoder_inputs, decoder_inputs, encoder_lengths, decoder_lengths = [], [], [], []
+
+        # Get a random batch of encoder and decoder inputs from data,
+        # pad them if needed, reverse encoder inputs and add GO to decoder.
+        for _ in range(self.batch_size):
+            encoder_input, decoder_input = random.choice(data)
+            encoder_size = len(encoder_input)
+            decoder_size = len(decoder_input)
+            encoder_lengths.append(encoder_size)
+            decoder_lengths.append(decoder_size)
+            # Encoder inputs are padded and then reversed.
+            encoder_pad = [data_utils.PAD_ID] * (self.max_length - encoder_size)
+            encoder_inputs.append(list(reversed(encoder_input + encoder_pad)))
+
+            # Decoder inputs get an extra "GO" symbol, and are padded then.
+            decoder_pad_size = self.max_length - decoder_size - 1
+            decoder_inputs.append([data_utils.GO_ID] + decoder_input +
+                                  [data_utils.PAD_ID] * decoder_pad_size)
+
+        # Now we create batch-major vectors from the data selected above.
+        batch_encoder_inputs, batch_decoder_inputs = [], []
+
+        # Batch encoder inputs are just re-indexed encoder_inputs.
+        for length_idx in range(self.max_length):
+            batch_encoder_inputs.append(
+                np.array([encoder_inputs[batch_idx][length_idx]
+                          for batch_idx in range(self.batch_size)], dtype=np.int32))
+            batch_decoder_inputs.append(
+                np.array([decoder_inputs[batch_idx][length_idx]
+                          for batch_idx in range(self.batch_size)], dtype=np.int32))
+
+        return batch_encoder_inputs, batch_decoder_inputs, \
+               np.array(encoder_lengths, dtype=np.int32), np.array(decoder_lengths, dtype=np.int32)
